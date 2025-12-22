4116ed31ad26a704e0b9cc8d92e86eab3a7ce082
Your Name <you@example.com>
2025-12-23 01:27:13 +0300

    Exclude TEMPLATE_KIT from git - local only

--- TEMPLATE_KIT/TEMPLATE_PROJECT/src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = ({ children, className = '' }) => (
-    <div className={`bg-white rounded-[2rem] p-6 md:p-8 shadow-soft border border-slate-100 hover:shadow-lg transition-shadow duration-300 ${className}`}>
-        {children}
-    </div>
-);
-
-export default Card;
